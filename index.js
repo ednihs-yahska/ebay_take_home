2181858ff305abdfefaa08f14b0a300472499852
Akshay <shindea@oregonstate.edu>
2021-07-03 22:47:59 -0700

    Adding css styles

--- index.js
+++ index.js
@@ -10,11 +10,11 @@
 
 function addRecentlyViewed(parent, items) {
     items.forEach((element, key) => {
-        const itemDiv = createDiv("item-"+key, "recently-viewed-item")
-        const itemImage = createImage("item-image-"+key, "recently-viewed-item-image", element.image)
-        const itemName = createTitle("item-name-"+key, "recently-viewed-item-name", element.title)
-        const itemPrice = createPrice("item-price-"+key, "recently-viewed-item-price", element.price)
-        const itemButton = createButton("btn-item-action-"+key,"recently-viewed-item-action", "Add to cart", ()=>{
+        const itemDiv = createDiv("item-"+key, ["recently-viewed-item"])
+        const itemImage = createImage("item-image-"+key,[ "recently-viewed-item-image"], element.image)
+        const itemName = createTitle("item-name-"+key, ["recently-viewed-item-name", "truncate-overflow"], element.title)
+        const itemPrice = createPrice("item-price-"+key, ["recently-viewed-item-price"], element.price)
+        const itemButton = createButton("btn-item-action-"+key,["recently-viewed-item-action"], "Add to cart", ()=>{
             console.log("Item "+key+" clicked")
         })
         itemDiv.appendChild(itemImage)
@@ -25,38 +25,46 @@ function addRecentlyViewed(parent, items) {
     });
 }
 
-function createDiv(id, className) {
+function createDiv(id, classNames) {
     const div = document.createElement("div")
     div.setAttribute("id", id)
-    div.classList.add(className)
+    classNames.forEach(className=>{
+        div.classList.add(className)
+    })
     return div
 }
 
-function createImage(id, className, src) {
+function createImage(id, classNames, src) {
+    const imageContainer = createDiv("img-container", ["image-container"])
     const image = document.createElement("img")
     image.setAttribute("id", id)
-    image.classList.add(className)
+    classNames.forEach(className=>{
+        image.classList.add(className)
+    })
     image.setAttribute("src", src)
-    return image
+    imageContainer.appendChild(image)
+    return imageContainer
 }
 
-function createTitle(id, className, title) {
-    const titleDiv = createDiv(id, className)
-    titleDiv.innerHTML = "<span class=\"spn-title\">"+title+"</span>"
+function createTitle(id, classNames, title) {
+    const titleDiv = createDiv(id, classNames)
+    titleDiv.innerHTML = "<span class=\"span-title\">"+title+"</span>"
     return titleDiv
 }
 
-function createPrice(id, className, price) {
-    const priceDiv = createDiv(id, className)
-    priceDiv.innerHTML = "<span class=\"spn-price\">"+price+"</span>"
+function createPrice(id, classNames, price) {
+    const priceDiv = createDiv(id, classNames)
+    priceDiv.innerHTML = "<span class=\"span-price\">"+price+"</span>"
     return priceDiv
 }
 
 
-function createButton(id, className, label, onClick) {
+function createButton(id, classNames, label, onClick) {
     const button = document.createElement("button")
     button.setAttribute("id", id)
-    button.classList.add(className)
+    classNames.forEach(className=>{
+        button.classList.add(className)
+    })
     button.addEventListener("click", onClick)
     button.innerText = label
     return button
